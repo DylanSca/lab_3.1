5d81bd8749d690161fef3610d247303d4754458b
Dylan <rockrz91919@gmail.com>
2025-09-14 22:59:36 -0400

    16-20 finshed project

---
+++ testing.java
@@ -0,0 +1,54 @@
+
+/**
+ * Write a description of class testing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class testing
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int a;
+    private int b;
+    /**
+     * Constructor for objects of class testing
+     */
+    public testing()
+    {
+        // initialise instance variables
+        x = 0;
+        a= 1;
+        b=5;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public boolean testing(){
+    
+        if(a>b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+        
+    public String returntest(){
+        return 9+3+"cat";
+    }
+    
+    public String returntest2(){
+        return "cat" +9+3;
+    }
+    }
